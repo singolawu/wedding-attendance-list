64b230cd5ddbd78b837a94cb8cfde7b5fcd99d67
singolawu <singolawu.corp@gmail.com>
2020-03-13 18:33:22 +0700

    Updates

---
+++ precache-manifest.3cda4ccf2e2dfdf64ca3b322180e0075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcc447a9fd1fa367a60e95b8eae63c0",
+    "url": "/wedding-attendance-list/index.html"
+  },
+  {
+    "revision": "ffe1ac308862dde4b120",
+    "url": "/wedding-attendance-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51df8dc924ed9ae553e2",
+    "url": "/wedding-attendance-list/static/js/2.9d840f1c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/wedding-attendance-list/static/js/2.9d840f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe1ac308862dde4b120",
+    "url": "/wedding-attendance-list/static/js/main.cc3d86a7.chunk.js"
+  },
+  {
+    "revision": "6ca711a41c53d6433c67",
+    "url": "/wedding-attendance-list/static/js/runtime-main.2d9065bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wedding-attendance-list/static/media/logo.5d5d9eef.svg"
+  }
+]);

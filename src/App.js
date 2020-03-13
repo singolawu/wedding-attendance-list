5446f1d04f4c227f8065a34f4e967419e5020f1f
singolawu <singolawu.corp@gmail.com>
2020-03-13 19:26:48 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Firebase from 'firebase';
+import config from './config';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Firebase.initializeApp(config);
+    this.getData()
+  }
+
+  getData = () => {
+    let ref = Firebase.database().ref('/wedding-attendance');
+    ref.on('value', snapshot => {
+      const state = this.snapshotToArray(snapshot)
+      console.log({ state })
+      this.setState({
+        data: state
+      })
+    });
+
+  }
+
+  snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function (childSnapshot) {
+      var item = childSnapshot.val();
+      item.key = childSnapshot.key;
+
+      returnArr.push(item);
+    });
+
+    return returnArr;
+  };
+
+  transitionChanged = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  indexChanged = index => {
+    this.setState({ index });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <table style={{ width: '100%', paddingLeft: 20, paddingRight: 20 }}>
+          <thead style={{ textAlign: 'center' }}>
+            <tr style={{ width: '100%'}}>
+              <th colSpan={3}>
+                Daftar Hadir Pernikahan
+              </th>
+            </tr>
+            <tr>
+              <th style={{ width: '20%' }}>
+                Nama
+              </th>
+              <th style={{ width: '60%' }}>
+                Ucapan/Doa
+              </th>
+              <th style={{ width: '20%' }}>
+                Kehadiran
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((item) =>
+              < tr >
+                <td>
+                  <text>
+                    {item.name}
+                  </text>
+                </td>
+                <td>
+                  <text>
+                    {item.words}
+                  </text>
+                </td>
+                <td>
+                  <text>
+                    {item.attendance}
+                  </text>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
